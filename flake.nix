7173222b2731140f9e5cc9fc25c44e8a0d9aaac6
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-20 20:53:46 +0200

    Fix missing Sapling
    ghstack-source-id: 952f4a548511c3330c514c9917a3cd7a81c5cc62
    Pull Request resolved: https://github.com/shikanime-studio/sapling-action/pull/41

--- flake.nix
+++ flake.nix
@@ -28,8 +28,44 @@
       imports = [
         devenv.flakeModule
         treefmt-nix.flakeModule
-        ./nix/devenv.nix
       ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            enableDefaultExcludes = true;
+            programs = {
+              nixfmt.enable = true;
+              prettier.enable = true;
+              shfmt.enable = true;
+              statix.enable = true;
+            };
+            settings.global.excludes = [
+              ".devenv/*"
+              ".direnv/*"
+              ".sl/*"
+              "LICENSE"
+            ];
+          };
+          devenv.shells.default = {
+            containers = pkgs.lib.mkForce { };
+            languages.nix.enable = true;
+            cachix = {
+              enable = true;
+              push = "shikanime";
+            };
+            git-hooks.hooks = {
+              actionlint.enable = true;
+              deadnix.enable = true;
+              flake-checker.enable = true;
+            };
+            packages = [
+              pkgs.gh
+              pkgs.sapling
+            ];
+          };
+        };
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
