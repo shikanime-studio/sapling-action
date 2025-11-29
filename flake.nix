eb2a7db5d6060d3dd867e3d0e3663fd879e9561f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-29 13:47:17 +0100

    Upgrade devlib

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
+    automata.url = "github:shikanime-studio/automata";
     devenv.url = "github:cachix/devenv";
     devlib.url = "github:shikanime-studio/devlib";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks.url = "github:cachix/git-hooks.nix";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
@@ -25,60 +27,22 @@
       devenv,
       devlib,
       flake-parts,
+      git-hooks,
       treefmt-nix,
       ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         devenv.flakeModule
+        devlib.flakeModule
+        git-hooks.flakeModule
         treefmt-nix.flakeModule
       ];
-      perSystem =
-        { pkgs, ... }:
-        {
-          treefmt = {
-            projectRootFile = "flake.nix";
-            enableDefaultExcludes = true;
-            programs = {
-              nixfmt.enable = true;
-              prettier.enable = true;
-              shfmt.enable = true;
-              statix.enable = true;
-            };
-            settings.global.excludes = [
-              ".devenv/*"
-              ".direnv/*"
-              ".sl/*"
-              "LICENSE"
-            ];
-          };
-          devenv = {
-            modules = [
-              devlib.devenvModule
-            ];
-            shells.default = {
-              containers = pkgs.lib.mkForce { };
-              languages.nix.enable = true;
-              cachix = {
-                enable = true;
-                push = "shikanime";
-              };
-              git-hooks.hooks = {
-                actionlint.enable = true;
-                deadnix.enable = true;
-                flake-checker.enable = true;
-              };
-              gitignore = {
-                enable = true;
-                enableDefaultTemplates = true;
-              };
-              packages = [
-                pkgs.gh
-                pkgs.sapling
-              ];
-            };
-          };
-        };
+      perSystem = _: {
+        devenv.shells.default.imports = [
+          devlib.devenvModules.shikanime-studio
+        ];
+      };
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
